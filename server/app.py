e91569f6790816b9de09fbeb14cb61ae1dcd36a9
byron korir <byron.korir@student.moringaschool.com>
2024-01-04 18:05:36 +0300

    complete assignment

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = True
 
@@ -17,10 +17,40 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(jsonify(plants_dict_list),200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    def post(self):
+        data = request.get_json()
+
+        new_plant = Plant(
+            name = data['name'],
+            image = data['image'],
+            price = data['price'],
+        )  
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_body = new_plant.to_dict()
+        response =  make_response(jsonify(response_body),201)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(jsonify(plant),200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
